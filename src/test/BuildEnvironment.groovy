89912f2da88420f36c7b49c9b808ed3149a480ee
marcbperez <marcbperez@users.noreply.github.com>
2017-03-13 21:59:42 +0000

    Added Dockerfile test case for the build environment.

--- src/test/BuildEnvironment.groovy
+++ src/test/BuildEnvironment.groovy
@@ -10,4 +10,9 @@ class BuildEnvironmentSpec extends Specification {
     expect:
       new File('docker-compose.yml').exists()
   }
+
+  def "Dockerfile exists"() {
+    expect:
+      new File('Dockerfile').exists()
+  }
 }
